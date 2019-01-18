be12671cdecdf647d102ebfd0f6da9135223ddd6
Diego Azurdia <azu17470@uvg.edu.gt>
2019-01-17 18:29:22 -0600

    Pruebas unitarias

---
+++ src/HdT1Tests/RadioJD.java
@@ -0,0 +1,124 @@
+package HdT1Tests;
+
+
+public class RadioJD implements Radio{
+    //Atributos
+    private boolean isOn = false;
+    private boolean isFM = true;
+    private double emisora = 87.0;
+    private Emisora[] ArrayDeEmisoras = new Emisora[11];
+
+    //CONSTANTES
+    public static final double FM_STATION_INCREMENT = 0.5;
+    public static final double AM_STATION_INCREMENT = 100.0;
+    public static final double FM_MIN_STATION = 87.0;
+    public static final double FM_MAX_STATION = 104.0;
+    public static final double AM_MIN_STATION = 1000.0;
+    public static final double AM_MAX_STATION = 4000.0;
+
+    //Metodos
+
+    public boolean encendidoRadio(){
+        this.isOn = !this.isOn;
+        return this.isOn;
+    }
+
+    public double subirFrecuencia(){
+        if (this.isFM){
+            this.emisora+= FM_STATION_INCREMENT;
+            if (this.emisora>= FM_MAX_STATION) {
+                this.emisora = FM_MIN_STATION;
+            }
+        } else {
+            this.emisora+= AM_STATION_INCREMENT;
+            if (this.emisora>= AM_MAX_STATION) {
+                this.emisora= AM_MIN_STATION;
+            }
+        }
+        return this.emisora;
+    }
+
+    public double bajarFrecuencia(){
+        if (this.isFM) {
+            this.emisora-= FM_STATION_INCREMENT;
+            if (this.emisora<= FM_MIN_STATION) {
+                this.emisora= FM_MAX_STATION;
+            }
+        } else {
+            this.emisora-= AM_STATION_INCREMENT;
+            if (this.emisora<= AM_MIN_STATION) {
+                this.emisora= AM_MAX_STATION;
+            }
+        }
+        return this.emisora;
+    }
+
+    public void setFavorito(int posicion){
+        Emisora emisora = new Emisora();
+        emisora.setEstacion(getEmisora());
+        if (this.isFM){
+            emisora.setFrecuencia("FM");
+        } else {
+          emisora.setFrecuencia("AM");
+        }
+        this.ArrayDeEmisoras[posicion - 1] = emisora;
+    }
+
+    public double getFavorito(int posicion){
+        if (ArrayDeEmisoras[posicion - 1] != null){
+            setEmisora(ArrayDeEmisoras[posicion - 1].getEstacion());
+            setIsFM(ArrayDeEmisoras[posicion - 1].getFrecuencia());
+            return this.emisora;
+        }
+        return this.emisora ;
+    }
+
+    public boolean cambiarAmFm(){
+        this.isFM = !isFM;
+        setEmisora();
+        return this.isFM;
+    }
+
+    public void setEmisora(){
+        if (this.isFM){
+            this.emisora = FM_MIN_STATION;
+        } else {
+            this.emisora = AM_MIN_STATION;
+        }
+    }
+
+    public void setEmisora(double _emisora){
+        this.emisora = _emisora;
+    }
+
+    public void setIsFM(String isFM){
+        if(isFM.equals("FM")){
+            this.isFM = true;
+        } else {
+            this.isFM = false;
+        }
+    }
+
+
+    public double getEmisora() {
+        return emisora;
+    }
+
+    public boolean isTurnedOn(){
+        return this.isOn;
+    }
+
+    @Override
+    public String toString(){
+        if (this.isOn){
+            return "\nRadio:" +
+                    "\nIs on: " + this.isOn +
+                    "\nIs FM: " + this.isFM +
+                    "\nEstacion: " + this.emisora;
+        }
+
+        return "Radio:\n\n" +
+                "Is on: " + this.isOn;
+
+    }
+}
